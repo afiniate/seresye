e033f9dc0dfe8e9afeaed07a638b9da9e0e8f9dc
cabdou <clementine.abdou@capgemini.com>
2016-04-28 14:32:15 +0200

    rebar config
    Fix  Warning: erlang:now/0: Deprecated BIF

--- src/seresye_speedtest.erl
+++ src/seresye_speedtest.erl
@@ -20,17 +20,17 @@ final_rule(Engine0, {is, started} = X)
     seresye_engine:assert (Engine1, {is, finished}).
 
 run_sieve() ->
-    Start = now(),
+    Start = erlang:system_time(),
     Engine1 = seresye_engine:assert (seresye_engine:new(),
                              [{X} || X <- lists:seq (2, 200)]),
     Engine2 = seresye_engine:add_rule (Engine1, {?MODULE, remove_multiple}, 2),
     Engine3 = seresye_engine:add_rule (Engine2, {?MODULE, final_rule}, 1),
     Engine4 = seresye_engine:assert (Engine3, {is, started}),
-    End = now(),
+    End = erlang:system_time(),
     io:format("Kb ~p~n", [seresye_engine:get_kb (Engine4)]),
     R = seresye_engine:get_rules_fired (Engine4),
     io:format ("Rules fired: ~p~n", [R]),
-    D = timer:now_diff(End, Start),
+    D = End - Start,
     io:format ("Time = ~p sec, ~p rules/sec, rule execution time ~p msec~n",
                [D / 1000000.0,
                 R / (D / 1000000.0),
